bf81fa9a0188eb3cc68b5fb9d1e5f33b62bf4fcd
Nedu-ds <69487742+Nedu-ds@users.noreply.github.com>
2022-07-01 11:28:33 -0500

    Delete seguridad_informatica directory

--- seguridad_informatica/config.py
+++
@@ -1,8 +0,0 @@
-class Config(object):
-    SECRET_KEY = 'MFRSTWBPG@PYTHN'
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/accesos'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/flask'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
